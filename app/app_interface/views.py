7a5eab2e8d86f3f3355c0df446e777adcf50e11d
mkorman <michal.korman@icloud.com>
2018-02-09 13:10:15 +0100

    simplify settings

--- app/app_interface/views.py
+++ app/app_interface/views.py
@@ -1,5 +1,5 @@
+from django.http import JsonResponse
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
 
 from utils import printer
 
@@ -7,4 +7,4 @@ from utils import printer
 @api_view(['GET'])
 def root(request, format=None):
     printer.hello_world()
-    return Response(status=200, data="Hello world")
+    return JsonResponse(status=200, data="Hello world", safe=False)
