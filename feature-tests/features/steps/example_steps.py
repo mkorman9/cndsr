e3d6b24fc5ec12beb219e51020fdac8886c0975e
mkorman9 <michal.korman@icloud.com>
2018-02-10 15:36:54 +0100

    implement feature tests in behave

---
+++ feature-tests/features/steps/example_steps.py
@@ -0,0 +1,31 @@
+import os
+from collections import namedtuple
+
+import requests
+from behave import when, then, given
+
+ServiceLocation = namedtuple('ServiceLocation', ['host', 'port'])
+
+
+@given('a service')
+def step_impl(context):
+    context.service_location = ServiceLocation(
+        host=os.getenv("APP_SERVICE_HOST"),
+        port=os.getenv("APP_SERVICE_PORT")
+    )
+
+    assert context.service_location.host is not None
+    assert context.service_location.port is not None
+
+
+@when('we request {endpoint} endpoint')
+def step_impl(context, endpoint):
+    context.response = requests.get(
+        'http://{}:{}/'.format(context.service_location.host, context.service_location.port)
+    )
+
+
+@then('{content} should be returned')
+def step_impl(context, content):
+    context.response.raise_for_status()
+    assert context.response.json() == "Hello world"
