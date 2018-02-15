011d53a3b70c6caec578f969d592c262be252b08
mkorman9 <michal.korman@icloud.com>
2018-02-15 20:27:20 +0100

    change font to Roboto

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -4,7 +4,9 @@ import '../index.css';
 class About extends Component {
   render() {
     return (
-      <div>About</div>
+      <div>
+        <p>cndsr (condenser) is a simple URL shortening service. It's completely free and open source.</p>
+      </div>
     );
   }
 }
